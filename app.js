5d02733c2daf1a78cafa099f74bf106e04ac728d
Katie Wcisel <kwcisel@gmail.com>
2024-07-18 22:49:15 -0700

    testing input

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 app.post("/inquiry", async (req, res) => {
-  console.log(req);
+  console.log(req.body);
   const headers = req.headers;
-  const response = await fetch(
+  console.log(headers);
+  /*const response = await fetch(
       "https://api.lodgify.com/v1/reservation/enquiry",
       {
           method: "POST",
           headers: { ...headers, "X-ApiKey": process.env.X_APIKEY },
-          body: JSON.stringify(body),
+          body: JSON.stringify(req.body),
       }
   )
 
   if (!response.ok) {
     return res.status(500).send('failure');
-  }
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  return response;
+  }*/
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  return res.status(200).send('success');
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
