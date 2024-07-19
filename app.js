b26406f969eb68b8d1c1b45133b59e574c518752
Katie Wcisel <kwcisel@gmail.com>
2024-07-19 00:01:54 -0700

    adding cors package

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cors());
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
