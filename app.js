4aa947c95da4fc9c797412fb4959b8e86f34f3ce
Katie Wcisel <kwcisel@gmail.com>
2024-07-18 22:04:12 -0700

    adding post

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.post("/", (req, res) => {
+  return res.send('success');
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
